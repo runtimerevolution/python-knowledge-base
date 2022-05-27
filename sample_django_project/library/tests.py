8b2f2f843f5f8109a04d3f6a8b302032553ed591
António Caeiro <ac.caeiro.general@gmail.com>
2022-05-27 16:33:59 +0100

    test pre-commit

--- sample_django_project/library/tests.py
+++ sample_django_project/library/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 class SampleTest(TestCase):
     """Test to be deleted simply to test pre-commit"""
 
-    def test_nothing(self):
-        pass
-        a = "I am a very very lsdknf lkjasd nlkajsdbflk sdjbfklajsdbfkjdbfkljsdabf lkdbflkasjdbfakjsdbflahsbdfbhvsdfhb"
+    def test_pre_commit(self):
+        # test flake8 and black allow more than 88 chars less than 120
+        a = "I am a very very lsdknf lkjasd nlkajsdbflk sdjbfklajsdbfkjdbfkljsdabf lkdbflkasjdbfakjsdbflahsbdfbhvsdfhba"
         print(a)
