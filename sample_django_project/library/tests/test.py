83c1d97e27a34da51180fe4a90df8ecf95758a58
João Gomes <j.gomes@runtime-revolution.com>
2022-09-30 11:28:50 +0100

    - tests updated - doc mock

--- sample_django_project/library/tests/test.py
+++ sample_django_project/library/tests/test.py
@@ -1,9 +1,11 @@
 from datetime import datetime
-from unittest.mock import patch
 
 from django.test import TestCase
 
-from ..utils import yesterday
+from ..utils import yesterday, timezone
+from unittest.mock import patch, MagicMock, Mock
+
+
 
 
 class SampleTest(TestCase):
@@ -19,6 +21,24 @@ class YesterdayTest(TestCase):
     @patch("library.utils.timezone.now")
     def test_success(self, mock):
         mock.return_value = datetime(1945, 2, 12, 0, 0, 0)
+
+        expected = datetime(1945, 2, 11).date()
+
+        self.assertEqual(yesterday(), expected)
+
+class YesterdayTestMagicMock(TestCase):
+    def test_success(self):
+        tz = timezone
+        tz.now = MagicMock(return_value=datetime(1945, 2, 12, 0, 0, 0))
+
+        expected = datetime(1945, 2, 11).date()
+
+        self.assertEqual(yesterday(), expected)
+
+class YesterdayTestMock(TestCase):
+    def test_success(self):
+        tz = timezone
+        tz.now = Mock(return_value=datetime(1945, 2, 12, 0, 0, 0))
 
         expected = datetime(1945, 2, 11).date()
 
